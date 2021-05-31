8ff0fc2a45f1f9e73a6606101e63c2a3a65670a2
Michiel <michielbouwmeester@gmail.com>
2021-05-31 21:32:23 +0200

    made contact model, migration, routing to test

---
+++ database/migrations/2021_05_31_190819_create_contacts_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateContactsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('contacts', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->string('first_name');
+            $table->string('last_name');
+            $table->string('street');
+            $table->string('house_nr');
+            $table->string('zip_code');
+            $table->string('city');
+            $table->string('email');
+            $table->integer('phone_nr');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('contacts');
+    }
+}
